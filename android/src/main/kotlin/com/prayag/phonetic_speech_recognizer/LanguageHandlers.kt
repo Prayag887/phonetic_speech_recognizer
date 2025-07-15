4474ab45167786c4756288c466121b03e8fff1e2
Prayag <prayag@PrayagtekiMac-mini.local>
2025-07-15 10:30:55 +0545

    threshold Changes

--- android/src/main/kotlin/com/prayag/phonetic_speech_recognizer/LanguageHandlers.kt
+++ android/src/main/kotlin/com/prayag/phonetic_speech_recognizer/LanguageHandlers.kt
@@ -373,13 +373,13 @@ class LanguageHandlers(private val context: Context) {
         return when {
             wordCount == 1 -> 0.40
             wordCount < 2 && hasProblematicWords -> 0.50
-            wordCount == 2 -> 0.50
-            wordCount <= 2 && hasShortWords -> 0.50
+            wordCount == 2 -> 0.60
+            wordCount <= 2 && hasShortWords -> 0.60
             wordCount <= 3 && hasMultiSyllableWords -> 0.60  // New condition for multi-syllable words
             wordCount <= 3 && hasProblematicWords -> 0.50
-            wordCount <= 3 -> 0.50
+            wordCount <= 3 -> 0.60
             wordCount >= 4 -> 0.60
-            hasProblematicWords -> 0.50
+            hasProblematicWords -> 0.60
             else -> 0.60
         }
     }
