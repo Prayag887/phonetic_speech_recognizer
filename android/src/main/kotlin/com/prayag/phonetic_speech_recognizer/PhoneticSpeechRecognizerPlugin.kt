e8d1e4020582313cd4379cb120c8083b989b4c22
Prayag <prayag@PrayagtekiMac-mini.local>
2025-08-27 13:14:17 +0545

    return error on silence

--- android/src/main/kotlin/com/prayag/phonetic_speech_recognizer/PhoneticSpeechRecognizerPlugin.kt
+++ android/src/main/kotlin/com/prayag/phonetic_speech_recognizer/PhoneticSpeechRecognizerPlugin.kt
@@ -1655,7 +1655,7 @@ class PhoneticSpeechRecognizerPlugin : FlutterPlugin, MethodChannel.MethodCallHa
     timeoutHandler?.removeCallbacks(timeoutRunnable!!)
     timeoutHandler = null
     timeoutRunnable = null
-    activeResult = null
+    activeResult?.success("ERROR")
     speechRecognizer?.cancel()
 
     isListening = false
